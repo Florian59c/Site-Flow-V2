650a3e1d838daec758f2f86b5e5f5b353dd5df8e
Florian59c <florian.cagnon@live.fr>
2024-11-16 15:22:43 +0100

    creation of NotFound page

--- src/screens/NotFound.jsx
+++ src/screens/NotFound.jsx
@@ -1,9 +1,16 @@
 import './css/NotFound.scss';
+import { Link } from 'react-router-dom';
+import Button from "assets/img/Button.png";
 
 function NotFound() {
     return (
         <div className="NotFound">
-            <p>404</p>
+            <h1>404</h1>
+            <h2>La page que vous cherchez n'existe pas !</h2>
+            <Link to="/" className='button'>
+                <img src={Button} alt='bouton personnalisé' />
+                <h3>Retourner à la page d'accueil</h3>
+            </Link>
         </div>
     );
 }
