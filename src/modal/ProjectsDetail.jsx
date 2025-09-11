89e7a5481f70f47ce14e1929176163c488c11281
Florian Cagnon <florian.cagnon@live.fr>
2025-09-11 14:12:51 +0200

    added a button for the CV and a modal displaying the CVs and a download link in the profile component

--- src/modal/ProjectsDetail.jsx
+++ src/modal/ProjectsDetail.jsx
@@ -1,12 +1,25 @@
 import './css/ProjectsDetail.scss';
+import { useState } from 'react';
 import X from "assets/img/icon/X.png";
 import Button from "assets/img/Button.png";
 
 function ProjectsDetail({ selectedProject, setOpenModal }) {
+    const [closing, setClosing] = useState(false);
+
+    const handleClose = () => {
+        setClosing(true);
+        setTimeout(() => setOpenModal(false), 500);
+    };
+
     return (
-        <div className="ProjectsDetail" data-aos="zoom-in" data-aos-duration="200">
-            <div className='container'>
-                <div className='x' onClick={() => setOpenModal(false)}>
+        <div
+            className={`ProjectsDetail ${closing ? "closing" : ""}`}
+            data-aos="zoom-in"
+            data-aos-duration="500"
+            onClick={handleClose}
+        >
+            <div className='container' onClick={(e) => e.stopPropagation()}>
+                <div className='x' onClick={handleClose}>
                     <img src={X} alt="Fermer la fenêtre" />
                 </div>
                 <div className='content'>
@@ -18,41 +31,38 @@ function ProjectsDetail({ selectedProject, setOpenModal }) {
                         </div>
                         <p className='present'>{selectedProject.presentation}</p>
                     </div>
-                    {selectedProject.details &&
+                    {selectedProject.details && (
                         <div className='details'>
                             <h2>Plus de détails</h2>
                             <p>{selectedProject.details}</p>
                         </div>
-                    }
-                    {selectedProject.video.length !== 0 &&
+                    )}
+                    {selectedProject.video.length !== 0 && (
                         <div className='demo'>
                             <h3>Vidéos de Démonstration :</h3>
                             <div className='video-container'>
-                                {selectedProject.video.map((video, index) => {
-                                    return (
-                                        <div key={index} >
-                                            <video src={video.url} type="video/mp4" controls></video>
-                                            <p>{video.name}</p>
-                                        </div>
-                                    );
-                                })}
+                                {selectedProject.video.map((video, index) => (
+                                    <div key={index}>
+                                        <video src={video.url} type="video/mp4" controls></video>
+                                        <p>{video.name}</p>
+                                    </div>
+                                ))}
                             </div>
                         </div>
-                    }
-                    {selectedProject.links.length !== 0 &&
+                    )}
+                    {selectedProject.links.length !== 0 && (
                         <div className='links'>
                             <h2>Liens</h2>
                             <div className='buttons-container'>
-                                {selectedProject.links.map((link, index) => {
-                                    return (
-                                        <a href={link.url} target="_blank" rel="noreferrer noopener" key={index} className='link'>
-                                            <img src={Button} alt='bouton personnalisé' />
-                                            <h2>{link.name}</h2>
-                                        </a>
-                                    );
-                                })}</div>
+                                {selectedProject.links.map((link, index) => (
+                                    <a href={link.url} target="_blank" rel="noreferrer noopener" key={index} className='link'>
+                                        <img src={Button} alt='bouton personnalisé' />
+                                        <h2>{link.name}</h2>
+                                    </a>
+                                ))}
+                            </div>
                         </div>
-                    }
+                    )}
                 </div>
             </div>
         </div>
