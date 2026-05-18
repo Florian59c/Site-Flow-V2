8c696166f92305640a924a888d3685ba0f1287d8
Florian Cagnon <florian.cagnon@live.fr>
2026-05-18 17:21:23 +0200

    add skills and update of amazonlike project

--- src/data/SkillsList.jsx
+++ src/data/SkillsList.jsx
@@ -15,6 +15,9 @@ import Docker from "assets/img/logo/docker.png";
 import Jest from "assets/img/logo/jest.png";
 import Figma from "assets/img/logo/figma.png";
 import Jira from "assets/img/logo/jira.png";
+import Java from "assets/img/logo/java.png";
+import SpringBoot from "assets/img/logo/springboot.png";
+import Angular from "assets/img/logo/angular.png";
 import MUI from "assets/img/logo/material-ui.png";
 import TypeORM from "assets/img/logo/typeorm.png";
 import Bootstrap from "assets/img/logo/bootstrap.png";
@@ -22,116 +25,131 @@ import AOS from "assets/img/logo/aos.png";
 import Symfony from "assets/img/logo/symfony.png";
 
 const SkillsList = [
-    {
-        name: "React",
-        link: React,
-        isMainSkill: true
-    },
-    {
-        name: "Next.js",
-        link: Next,
-        isMainSkill: true
-    },
-    {
-        name: "JavaScript",
-        link: JS,
-        isMainSkill: true
-    },
-    {
-        name: "TypeScript",
-        link: TS,
-        isMainSkill: true
-    },
-    {
-        name: "SASS",
-        link: SASS,
-        isMainSkill: true
-    },
-    {
-        name: "Material UI",
-        link: MUI,
-        isMainSkill: true
-    },
-    {
-        name: "Node.js",
-        link: NodeJS,
-        isMainSkill: true
-    },
-    {
-        name: "Nest.js",
-        link: Nest,
-        isMainSkill: true
-    },
-    {
-        name: "TypeORM",
-        link: TypeORM,
-        isMainSkill: true
-    },
-    {
-        name: "Postgre SQL",
-        link: PostgreSQL,
-        isMainSkill: true
-    },
-    {
-        name: "GitHub",
-        link: GitHub,
-        isMainSkill: true
-    },
-    {
-        name: "Docker",
-        link: Docker,
-        isMainSkill: true
-    },
-    {
-        name: "Jest",
-        link: Jest,
-        isMainSkill: true
-    },
-    {
-        name: "Figma",
-        link: Figma,
-        isMainSkill: true
-    },
-    {
-        name: "Jira",
-        link: Jira,
-        isMainSkill: true
-    },
-    {
-        name: "Tailwind",
-        link: Tailwind,
-        isMainSkill: false
-    },
-    {
-        name: "Bootstrap",
-        link: Bootstrap,
-        isMainSkill: false
-    },
-    {
-        name: "AOS",
-        link: AOS,
-        isMainSkill: false
-    },
-    {
-        name: "GraphQL",
-        link: GraphQL,
-        isMainSkill: false
-    },
-    {
-        name: "PHP",
-        link: PHP,
-        isMainSkill: false
-    },
-    {
-        name: "Symfony",
-        link: Symfony,
-        isMainSkill: false
-    },
-    {
-        name: "GitLab",
-        link: GitLab,
-        isMainSkill: false
-    }
+  {
+    name: "React",
+    link: React,
+    isMainSkill: true
+  },
+  {
+    name: "Next.js",
+    link: Next,
+    isMainSkill: true
+  },
+  {
+    name: "JavaScript",
+    link: JS,
+    isMainSkill: true
+  },
+  {
+    name: "TypeScript",
+    link: TS,
+    isMainSkill: true
+  },
+  {
+    name: "SASS",
+    link: SASS,
+    isMainSkill: true
+  },
+  {
+    name: "Material UI",
+    link: MUI,
+    isMainSkill: true
+  },
+  {
+    name: "Node.js",
+    link: NodeJS,
+    isMainSkill: true
+  },
+  {
+    name: "Nest.js",
+    link: Nest,
+    isMainSkill: true
+  },
+  {
+    name: "TypeORM",
+    link: TypeORM,
+    isMainSkill: true
+  },
+  {
+    name: "Postgre SQL",
+    link: PostgreSQL,
+    isMainSkill: true
+  },
+  {
+    name: "GitHub",
+    link: GitHub,
+    isMainSkill: true
+  },
+  {
+    name: "Docker",
+    link: Docker,
+    isMainSkill: true
+  },
+  {
+    name: "Jest",
+    link: Jest,
+    isMainSkill: true
+  },
+  {
+    name: "Figma",
+    link: Figma,
+    isMainSkill: true
+  },
+  {
+    name: "Jira",
+    link: Jira,
+    isMainSkill: true
+  },
+  {
+    name: "Java",
+    link: Java,
+    isMainSkill: false
+  },
+  {
+    name: "Spring Boot",
+    link: SpringBoot,
+    isMainSkill: false
+  },
+  {
+    name: "Angular",
+    link: Angular,
+    isMainSkill: false
+  },
+  {
+    name: "Tailwind",
+    link: Tailwind,
+    isMainSkill: false
+  },
+  {
+    name: "Bootstrap",
+    link: Bootstrap,
+    isMainSkill: false
+  },
+  {
+    name: "AOS",
+    link: AOS,
+    isMainSkill: false
+  },
+  {
+    name: "GraphQL",
+    link: GraphQL,
+    isMainSkill: false
+  },
+  {
+    name: "PHP",
+    link: PHP,
+    isMainSkill: false
+  },
+  {
+    name: "Symfony",
+    link: Symfony,
+    isMainSkill: false
+  },
+  {
+    name: "GitLab",
+    link: GitLab,
+    isMainSkill: false
+  }
 ];
 
 export default SkillsList;
